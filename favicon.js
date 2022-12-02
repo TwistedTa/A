5fa8cbdcf5dc1f3a4c819a454246c9f60aaa8eda
TwistedTa <119633320+TwistedTa@users.noreply.github.com>
2022-12-02 16:27:21 -0600

    Delete favicon.js

--- favicon.js
+++
@@ -1,4 +0,0 @@
-if (localStorage.getItem("checked")) document.querySelector("title").innerHTML = "Google"
-  
-document.querySelector("link[rel=icon]").href =((location.href.slice(-1) == "/") && (!location.href.match("\/gxmehub\/$"))? ".." : ".") + "/" + (localStorage.getItem("checked") ? "google.png" : "favicon.ico")
- 
